ade332d19c01c04e51722dc55319eca315b2b606
ashishjayamohan <46698969+ashishjayamohan@users.noreply.github.com>
2020-09-05 14:45:50 -0700

    Competition

--- 667/b.py
+++ 667/b.py
@@ -1,25 +1,19 @@
 t = int(input())
-def dist(a,b,x,y,n):
-    low = 999999999999999999
-    if((a+b)-(x+y)<n):
-        return x*y
-    for i in range(n):
-        if((a-i) >= x and (b-(n-i)) >= y):
-            current = (a-i)*(b-(n-i))
-            low = min(low, current)
-    return low
 answers = []
 for a in range(t):
     line = input().split()
-    if(" ".join(line) == "1000000000 1000000000 1 1 1000000000"):
-        answers.append(999999999)
-        continue
     a = int(line[0])
     b = int(line[1])
     x = int(line[2])
     y = int(line[3])
     n = int(line[4])
-    ans = dist(a,b,x,y,n)
+    ans = 10000000000000000
+    for i in range(0,2):
+        da = min(n, a-x)
+        db = min(n - da, b - y)
+        ans = min(ans, (a - da) * (b - db))
+        x,y = y,x
+        a,b = b,a
     answers.append(ans)
 for b in answers:
     print(str(b))
